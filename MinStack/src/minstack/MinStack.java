f4bd54be59fb4068f5549fdb714db17edc486816
jiBaiGe <yezhaoyu1025@qq.com>
2018-01-12 22:46:03 +0800

    improve

--- MinStack/src/minstack/MinStack.java
+++ MinStack/src/minstack/MinStack.java
@@ -7,7 +7,7 @@ import java.util.Stack;
 /*实现一个stack类，定义栈的数据结构,实现一个能够得到栈的最小元素的getMin函数。在该栈中，调用getMin、push及pop的时间复杂度都是O(1).
 	最小值思路：用一个辅助栈stack2记住每次入栈stack1的当前最小值:
 	在stack1入栈时，往stack2中加入当前最小值；stack1元素出栈时，
-	stack2也出栈一个元素。最小值从stack2中获取及栈顶元素。O(1)
+	stack2也出栈一个元素。最小值从stack2中获取及栈顶元素。O(1)。
   
  */
 public class MinStack {
